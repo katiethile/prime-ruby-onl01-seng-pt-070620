420ba2aa061003d1d5276103da62a6259cd441fb
Katie Le <lethikatie@gmail.com>
2020-07-16 23:04:19 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,10 @@
 def prime?(num)
   if num <= 1 
     return false
-end 
-i = 2
-while i < num 
-  if num % i = 0
+  end 
+  i = 2
+  while i < num 
+    if num % i -- 0
     return false 
   end 
   i += 1 
