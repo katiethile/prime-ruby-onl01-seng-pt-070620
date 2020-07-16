b8626b1512ad7b0e3e23e0ad2a594dbf5b0dbc1e
Katie Le <lethikatie@gmail.com>
2020-07-16 23:00:21 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(num)
+  n = 0
+
+  while n < num[n]
+    if num % n == 0
+      return false
+    end
+    if num[n] <= 2
+      return false
+    end
+    n = n + 1
+  end
+  return true
+end
